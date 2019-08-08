44990ea325ae930f68888db61b325e9ac8319ac0
mitnick <1102743539@qq.com>
2019-08-08 11:34:07 +0800

    添加Toast提示工具类

---
+++ util/ToastUtils.java
@@ -0,0 +1,131 @@
+import android.content.Context;
+import android.widget.Toast;
+
+
+/**
+ * 文件日志工具类
+ * <p>
+ * Created by chengzj 2019/08/08
+ */
+public class ToastUtil {
+    // Toast
+    private static Toast toast;
+
+    public static boolean isShow = true;
+    
+    /**
+     * 短时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showShort(Context context, CharSequence message) {
+        if (null == toast) {
+            toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        } else {
+            toast.setText(message);
+        }
+        if (isShow)
+            toast.show();
+    }
+
+    /**
+     * 短时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showShort(Context context, int message) {
+        if (null == toast) {
+            toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+            // toast.setGravity(Gravity.CENTER, 0, 0);
+        } else {
+            toast.setText(message);
+        }
+        if (isShow)
+            toast.show();
+    }
+    /**
+     * 长时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showLong(Context context, CharSequence message) {
+        if (null == toast) {
+            toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+            // toast.setGravity(Gravity.CENTER, 0, 0);
+        } else {
+            toast.setText(message);
+        }
+        if (isShow)
+            toast.show();
+    }
+    
+
+    /**
+     * 长时间显示Toast
+     *
+     * @param context
+     * @param message
+     */
+    public static void showLong(Context context, int message) {
+        if (null == toast) {
+            toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+            // toast.setGravity(Gravity.CENTER, 0, 0);
+        } else {
+            toast.setText(message);
+        }
+        if (isShow)
+            toast.show();
+    }
+
+    /**
+     * 自定义显示Toast时间
+     *
+     * @param context
+     * @param message
+     * @param duration
+     */
+    public static void show(Context context, CharSequence message, int duration) {
+        if (null == toast) {
+            toast = Toast.makeText(context, message, duration);
+            // toast.setGravity(Gravity.CENTER, 0, 0);
+        } else {
+            toast.setText(message);
+            toast.setDuration(duration);
+        }
+        if (isShow)
+            toast.show();
+    }
+    
+    /**
+     * 自定义显示Toast时间
+     *
+     * @param context
+     * @param message
+     * @param duration
+     */
+    public static void show(Context context, int message, int duration) {
+        if (null == toast) {
+            toast = Toast.makeText(context, message, duration);
+            // toast.setGravity(Gravity.CENTER, 0, 0);
+        } else {
+            toast.setText(message);
+            toast.setDuration(duration);
+        }
+        if (isShow)
+            toast.show();
+    }
+
+    /**
+     * Hide the toast, if any.
+     */
+    public static void hideToast() {
+        if (null != toast) {
+            toast.cancel();
+        }
+    }
+}
+
+    
