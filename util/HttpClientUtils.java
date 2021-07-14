3cd3944204729e82fb0ecd6c6944fd6fa7c53234
mitnick <1102743539@qq.com>
2021-07-14 18:30:08 +0800

    有个http请求工具类代码逻辑，抽出相同代码，优化结构

--- util/HttpClientUtils.java
+++ util/HttpClientUtils.java
@@ -1,6 +1,7 @@
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.text.TextUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,126 +16,33 @@ import java.util.concurrent.Executors;
 
 
 /**
- * @author chengzj
+ * @author cheng
  * 文件名：HttpClientUtils
- * 创建日期：2021/5/21 18:15
+ * 创建日期：2020/5/21 18:15
  * 描述：HttpURLConnection 的请求工具类，封装主线程子线程切换
  */
 public class HttpClientUtils {
     public static final String TAG = HttpClientUtils.class.getSimpleName();
 
-    public static void post(final String actionUrl, final Map<String, String> params,final Callback callback)  {
-        HyLog.i(TAG," Url : " + actionUrl);
+    public static void post(final String actionUrl, final Map<String, String> params, final ResponseCallback callback) {
+        HyLog.d(TAG, "post Url : " + actionUrl);
         Executors.newSingleThreadExecutor().execute(new Runnable() {
             @Override
             public void run() {
                 String result = null;
                 try {
-                    String contentType = "application/x-www-form-urlencoded";
                     URL uri = new URL(actionUrl);
-                    HttpURLConnection conn = (HttpURLConnection) uri.openConnection();
-                    conn.setReadTimeout(6 * 1000); // Cache max time
-                    conn.setConnectTimeout(6 * 1000);
-                    conn.setDoInput(true);// allow input
-                    conn.setDoOutput(true);// Allow output
-                    conn.setUseCaches(false); // cache is disable
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("connection", "keep-alive");
-                    conn.setRequestProperty("Charsert", "UTF-8");
-                    conn.setRequestProperty("Content-Type", contentType);
-                    StringBuilder sb = new StringBuilder();
-
-                    for (Map.Entry<String, String> entry : params.entrySet()) {
-                        if (sb.length() > 0) {
-                            sb.append("&");
-                        }
-                        sb.append(entry.getKey());
-                        sb.append("=");
-                        sb.append(URLEncoder.encode(entry.getValue()));
-                    }
-                    HyLog.i(TAG," params : " + sb.toString());
+                    HttpURLConnection conn = getPostHttpURLConnection(uri);
+                    StringBuilder sb = getPostStringBuilder(params);
                     OutputStream output = conn.getOutputStream();
                     output.write(sb.toString().getBytes());
                     output.flush();
-
                     int res = conn.getResponseCode();
                     InputStream in = null;
                     switch (res) {
                         case 200:
-                            in = conn.getInputStream();
-                            String line;
-                            StringBuilder sb2 = new StringBuilder();
-                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-                            while ((line = bufferedReader.readLine()) != null) {
-                                sb2.append(line);
-                            }
-                            in.close();
-                            result = sb2.toString();
+                            result = executeData(conn, in);
 //                            HyLog.i(TAG," result : " + result);
-                            callback.onSuccess(result);
-                            break;
-                        case 403:
-                            callback.onFailure(new IOException(res + " : request forbided"));
-                        case 404:
-                            callback.onFailure(new IOException(res + " : not fonud such address"));
-                        default:
-                            callback.onFailure(new IOException(res + " : undefine error"));
-                    }
-                }catch (IOException exception){
-                    exception.printStackTrace();
-                    callback.onFailure(exception);
-                }
-            }
-        });
-    }
-
-    public static void post(final String actionUrl, final Map<String, String> params,final SimpleResponseCallback callback)  {
-        HyLog.i(TAG," Url : " + actionUrl);
-        Executors.newSingleThreadExecutor().execute(new Runnable() {
-            @Override
-            public void run() {
-                String result = null;
-                try {
-                    String contentType = "application/x-www-form-urlencoded";
-                    URL uri = new URL(actionUrl);
-                    HttpURLConnection conn = (HttpURLConnection) uri.openConnection();
-                    conn.setReadTimeout(6 * 1000); // Cache max time
-                    conn.setConnectTimeout(6 * 1000);
-                    conn.setDoInput(true);// allow input
-                    conn.setDoOutput(true);// Allow output
-                    conn.setUseCaches(false); // cache is disable
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("connection", "keep-alive");
-                    conn.setRequestProperty("Charsert", "UTF-8");
-                    conn.setRequestProperty("Content-Type", contentType);
-                    StringBuilder sb = new StringBuilder();
-
-                    for (Map.Entry<String, String> entry : params.entrySet()) {
-                        if (sb.length() > 0) {
-                            sb.append("&");
-                        }
-                        sb.append(entry.getKey());
-                        sb.append("=");
-                        sb.append(URLEncoder.encode(entry.getValue()));
-                    }
-                    HyLog.i(TAG," params : " + sb.toString());
-                    OutputStream output = conn.getOutputStream();
-                    output.write(sb.toString().getBytes());
-                    output.flush();
-
-                    int res = conn.getResponseCode();
-                    InputStream in = null;
-                    switch (res) {
-                        case 200:
-                            in = conn.getInputStream();
-                            String line;
-                            StringBuilder sb2 = new StringBuilder();
-                            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-                            while ((line = bufferedReader.readLine()) != null) {
-                                sb2.append(line);
-                            }
-                            in.close();
-                            result = sb2.toString();
                             callback.sendSuccessMessage(result);
                             break;
                         case 403:
@@ -144,7 +52,7 @@ public class HttpClientUtils {
                         default:
                             callback.sendFailuerMessage(new IOException(res + " : undefine error"));
                     }
-                }catch (IOException exception){
+                } catch (Exception exception) {
                     exception.printStackTrace();
                     callback.sendFailuerMessage(exception);
                 }
@@ -152,11 +60,99 @@ public class HttpClientUtils {
         });
     }
 
-    public static String get(String actionUrl, Map<String, String> params) throws Exception {
-        String contentType = "application/x-www-form-urlencoded";
 
+    public static void post(final String actionUrl, final Map<String, String> params, final SimpleResponseCallback callback) {
+        HyLog.d(TAG, " post Url : " + actionUrl);
+        Executors.newCachedThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                String result = null;
+                try {
+                    URL uri = new URL(actionUrl);
+                    HttpURLConnection conn = getPostHttpURLConnection(uri);
+                    StringBuilder sb = getPostStringBuilder(params);
+                    OutputStream output = conn.getOutputStream();
+                    output.write(sb.toString().getBytes());
+                    output.flush();
+                    int res = conn.getResponseCode();
+                    InputStream in = null;
+                    switch (res) {
+                        case 200:
+                            result = executeData(conn, in);
+                            System.out.println(TAG + " result : " + result);
+                            callback.sendSuccessMessage(result);
+                            break;
+                        case 403:
+                            callback.sendFailuerMessage(new IOException(res + " : request forbided"));
+                        case 404:
+                            callback.sendFailuerMessage(new IOException(res + " : not fonud such address"));
+                        default:
+                            callback.sendFailuerMessage(new IOException(res + " : undefine error"));
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                    callback.sendFailuerMessage(exception);
+                }
+            }
+        });
+    }
+
+    public static void get(final String actionUrl, final Map<String, String> params, final SimpleResponseCallback callback) {
+        Executors.newCachedThreadPool().execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String contentType = "application/x-www-form-urlencoded";
+                    URL uri = new URL(getRequestUrl(actionUrl, params));
+                    HttpURLConnection conn = (HttpURLConnection) uri.openConnection();
+                    conn.setReadTimeout(5 * 1000); // Cache max time
+                    conn.setDoInput(true);// allow input
+                    conn.setDoOutput(true);// Allow output
+                    conn.setUseCaches(false); // cache is disable
+                    conn.setRequestMethod("GET");
+                    conn.setRequestProperty("connection", "keep-alive");
+                    conn.setRequestProperty("Charsert", "UTF-8");
+                    conn.setRequestProperty("Content-Type", contentType);
+                    int res = conn.getResponseCode();
+                    InputStream in = null;
+                    String result = null;
+                    switch (res) {
+                        case 200:
+                            result = executeData(conn, in);
+                            callback.sendSuccessMessage(result);
+                            break;
+                        case 403:
+                            callback.sendFailuerMessage(new IOException(res + " : request forbided"));
+                        case 404:
+                            callback.sendFailuerMessage(new IOException(res + " : not fonud such address"));
+                        default:
+                            callback.sendFailuerMessage(new IOException(res + " : undefine error"));
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static String executeData(HttpURLConnection conn, InputStream in) throws Exception {
+        in = conn.getInputStream();
+        String line;
+        StringBuilder sb2 = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        while ((line = bufferedReader.readLine()) != null) {
+            sb2.append(line);
+        }
+        in.close();
+        return sb2.toString();
+    }
+
+    public static String getRequestUrl(final String actionUrl, final Map<String, String> params) {
         StringBuilder sb = new StringBuilder();
         for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (TextUtils.isEmpty(entry.getValue())) {
+                continue;
+            }
             if (sb.length() > 0) {
                 sb.append("&");
             }
@@ -164,53 +160,55 @@ public class HttpClientUtils {
             sb.append("=");
             sb.append(URLEncoder.encode(entry.getValue()));
         }
+        HyLog.d(TAG, "get 参数 : " + sb.toString());
+        final String url;
         if (actionUrl.endsWith("?")) {
-            actionUrl += sb.toString();
+            url = actionUrl + sb.toString();
         } else {
-            actionUrl += "?" + sb.toString();
+            url = actionUrl + "?" + sb.toString();
         }
-        URL uri = new URL(actionUrl);
+        HyLog.d(TAG, " get 完整链接 : " + url);
+        return url;
+    }
+
+    public static HttpURLConnection getPostHttpURLConnection(URL uri) throws Exception {
+        String contentType = "application/x-www-form-urlencoded";
         HttpURLConnection conn = (HttpURLConnection) uri.openConnection();
-        conn.setReadTimeout(5 * 1000); // Cache max time
+        conn.setReadTimeout(6 * 1000); // Cache max time
+        conn.setConnectTimeout(6 * 1000);
         conn.setDoInput(true);// allow input
         conn.setDoOutput(true);// Allow output
         conn.setUseCaches(false); // cache is disable
-        conn.setRequestMethod("GET");
+        conn.setRequestMethod("POST");
         conn.setRequestProperty("connection", "keep-alive");
         conn.setRequestProperty("Charsert", "UTF-8");
         conn.setRequestProperty("Content-Type", contentType);
-
-        int res = conn.getResponseCode();
-        // Log.i("le_assistant","actionUrl:" +actionUrl+"  ResponseCode:"+res);
-        InputStream in = null;
-        String result = null;
-        switch (res) {
-            case 200:
-                in = conn.getInputStream();
-                String line;
-                StringBuilder sb2 = new StringBuilder();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in,"UTF-8"));
-                while ((line = bufferedReader.readLine()) != null) {
-                    sb2.append(line);
-                }
-                result = sb2.toString();
-                break;
-            case 403:
-//                throw new HttpResponseException(res, "request forbided");
-            case 404:
-//                throw new HttpResponseException(res, "not fonud such address");
-            default:
-                throw new Exception("undefine error:" + res);
-        }
-        return result;
+        return conn;
     }
 
-    public abstract static class SimpleResponseCallback  implements Callback{
+    public static StringBuilder getPostStringBuilder(Map<String, String> params) {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (TextUtils.isEmpty(entry.getValue())) {
+                continue;
+            }
+            if (sb.length() > 0) {
+                sb.append("&");
+            }
+            sb.append(entry.getKey());
+            sb.append("=");
+            sb.append(URLEncoder.encode(entry.getValue()));
+        }
+        HyLog.d(TAG, "post params : " + sb.toString());
+        return sb;
+    }
+
+    public abstract static class SimpleResponseCallback implements Callback {
         private Handler handler;
 
         public SimpleResponseCallback() {
             Looper looper = Looper.myLooper();
-            this.handler = new ResultHandler(this,looper);
+            this.handler = new ResultHandler(this, looper);
         }
 
         public void handleMessage(Message message) {
@@ -255,8 +253,19 @@ public class HttpClientUtils {
         }
     }
 
-    public interface Callback{
-        void onSuccess(String response);
+    public static abstract class ResponseCallback implements Callback {
+        void sendSuccessMessage(String result) {
+            onSuccess(result);
+        }
+
+        void sendFailuerMessage(Throwable throwable) {
+            onFailure((Exception) throwable);
+        }
+    }
+
+    public interface Callback {
+        void onSuccess(String responseJson);
+
         void onFailure(Exception error);
     }
 }
